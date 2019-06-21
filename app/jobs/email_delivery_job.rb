c0e8f9669537e04ef3931e14f8e065c49cdf8a4d
Kevin Dew <kevindew@me.com>
2019-06-21 13:07:37 +0100

    Force an error when sending emails
    To be used to see how email retrying works.

--- app/jobs/email_delivery_job.rb
+++ app/jobs/email_delivery_job.rb
@@ -4,5 +4,21 @@ class EmailDeliveryJob < ActionMailer::DeliveryJob
   # retry at 3s, 18s, 83s, 258s, 627s
   retry_on(Notifications::Client::RequestError,
            wait: :exponentially_longer,
-           attempts: 5)
+           attempts: 1)
+
+  def perform(mailer, mail_method, delivery_method, *args)
+    response_error = OpenStruct.new(
+      code: "501",
+      body: {
+        errors: [
+          {
+            error: "Testing Content Publisher error handling",
+            message: "Testing Content Publisher error handling"
+          }
+        ]
+      }.to_json
+    )
+
+    raise Notifications::Client::RequestError, response_error
+  end
 end
