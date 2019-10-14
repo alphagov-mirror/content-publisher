bc553b446715cfb6042d650b53f4be7023b5b69a
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2019-10-14 16:17:10 +0100

    Use root bypass_ids
    For testing with this branch -
    https://github.com/alphagov/publishing-api/compare/use-editions-auth-bypass?expand=1

--- app/services/preview_service/payload.rb
+++ app/services/preview_service/payload.rb
@@ -28,6 +28,7 @@ class PreviewService::Payload
       ],
       "links" => links,
       "access_limited" => access_limited,
+      "auth_bypass_ids" => [auth_bypass_id],
     }
     payload["change_note"] = edition.change_note if edition.major?
 
@@ -41,15 +42,17 @@ class PreviewService::Payload
 
 private
 
-  def access_limited
-    auth_bypass_id = PreviewAuthBypass.new(edition).auth_bypass_id
-    access_limited_ids = { "auth_bypass_ids" => [auth_bypass_id] }
+  def auth_bypass_id
+    PreviewAuthBypass.new(edition).auth_bypass_id
+  end
 
+  def access_limited
+    access_limit = {}
     if edition.access_limit
-      access_limited_ids["organisations"] = edition.access_limit_organisation_ids
+      access_limit["organisations"] = edition.access_limit_organisation_ids
     end
 
-    access_limited_ids
+    access_limit
   end
 
   def links
