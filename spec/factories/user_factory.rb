d8f63aaa276b0b28677494f9f79c34cc5bf117fe
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2019-12-06 12:22:23 +0000

    Add pre-release permission for test managing editor
    The managing editor trait for the test user removes the pre-release
    feature permission.  This adds it back.
    
    Co-authored-by: Jon Hallam <jonathan.hallam@digital.cabinet-office.gov.uk>

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -5,9 +5,10 @@ FactoryBot.define do
     name { "John Smith" }
     uid { SecureRandom.uuid }
     email { "someone@example.com" }
+    permissions { [User::PRE_RELEASE_FEATURES_PERMISSION] }
 
     trait :managing_editor do
-      permissions { [User::MANAGING_EDITOR_PERMISSION] }
+      permissions { [User::MANAGING_EDITOR_PERMISSION, User::PRE_RELEASE_FEATURES_PERMISSION] }
     end
   end
 end
