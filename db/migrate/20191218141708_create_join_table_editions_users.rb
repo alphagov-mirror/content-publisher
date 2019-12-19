23f20e93d50d88fede215a881ee8ed928d162be5
Graham Lewis <graham.lewis@digital.cabinet-office.gov.uk>
2019-12-19 08:03:24 +0000

    Fix Robucop issue

--- db/migrate/20191218141708_create_join_table_editions_users.rb
+++ db/migrate/20191218141708_create_join_table_editions_users.rb
@@ -1,7 +1,7 @@
 class CreateJoinTableEditionsUsers < ActiveRecord::Migration[6.0]
   def change
     create_join_table :editions, :users do |t|
-      t.index %I[edition_id, user_id], unique: true
+      t.index %I[edition_id user_id], unique: true
     end
   end
 end
