d56b3740ac5443dc92432ffd997bf7d914ec9e80
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2019-12-24 10:28:16 +0000

    Get a list of documents to import from Whitehall
    Updates the import:whitehall_migration rake task to get a list of
    relevant documents and creates a WhitehallMigration::DocumentImport
    record (in a pending state) for each document.
    
    The complexity here is due to Whitehall paginating the results.  We loop
    through each page until we find a page has fewer than the expected
    number of results.

--- lib/services.rb
+++ lib/services.rb
@@ -6,7 +6,7 @@ module Services
   def self.whitehall(options = {})
     GdsApi::Whitehall.new(
       Plek.find("whitehall-admin"),
-      { bearer_token: ENV["WHITEHALL_BEARER_TOKEN"] }.merge(options),
+      { bearer_token: ENV.fetch("WHITEHALL_BEARER_TOKEN", "example") }.merge(options),
     )
   end
 end
