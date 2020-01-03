5ae42c40b72c8421194b8ee8d87f25bb98a7438d
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2020-01-03 09:51:22 +0000

    Delete draft assets and variants that have no equivalents
    (Ideally these would be in two separate commits but by applying one,
    we get the other 'for free'.)
    
    All assets on the draft stack should be deleted, period.
    Some assets on the live stack should be deleted too; namely:
    
    - Any file attachment variants (there is no equivalent in Content
      Publisher for things like 'file attachment thumbnail')
    - Some image variants (there is no equivalent in Content Publisher
      for certain image sizes, e.g. "s216")

--- spec/lib/whitehall_importer/migrate_assets_spec.rb
+++ spec/lib/whitehall_importer/migrate_assets_spec.rb
@@ -14,6 +14,26 @@ RSpec.describe WhitehallImporter::MigrateAssets do
       expect { described_class.call(build(:whitehall_migration_document_import)) }.not_to raise_error
     end
 
+    it "should delete draft assets" do
+      asset = create(:whitehall_migration_asset_import)
+      delete_asset_request = stub_asset_manager_delete_asset(asset_id)
+      whitehall_import = build(:whitehall_migration_document_import, assets: [asset])
+
+      described_class.call(whitehall_import)
+      expect(delete_asset_request).to have_been_requested
+      expect(asset.state).to eq("removed")
+    end
+
+    it "should delete draft asset variants" do
+      asset = create(:whitehall_migration_asset_import, :for_image, variant: "s300")
+      delete_asset_request = stub_asset_manager_delete_asset(asset_id)
+      whitehall_import = build(:whitehall_migration_document_import, assets: [asset])
+
+      described_class.call(whitehall_import)
+      expect(delete_asset_request).to have_been_requested
+      expect(asset.state).to eq("removed")
+    end
+
     it "should redirect live attachments to their content publisher equivalents" do
       new_file_url = "https://asset-manager.gov.uk/NEW/847151/foo.pdf"
       file_attachment_asset = create(:file_attachment_asset,
@@ -30,6 +50,22 @@ RSpec.describe WhitehallImporter::MigrateAssets do
       described_class.call(whitehall_import)
       expect(redirect_request).to have_been_requested
       expect(asset.state).to eq("redirected")
+    end
+
+    it "should delete attachment variants even if they are live" do
+      file_attachment_asset = build(:file_attachment_asset, state: "live")
+      asset = create(:whitehall_migration_asset_import,
+                     :for_file_attachment,
+                     variant: "thumbnail",
+                     file_attachment_revision: build(:file_attachment_revision,
+                                                     blob_revision: file_attachment_asset.blob_revision),
+                     original_asset_url: original_asset_url)
+      whitehall_import = build(:whitehall_migration_document_import, assets: [asset])
+      delete_request = stub_asset_manager_delete_asset(asset_id)
+
+      described_class.call(whitehall_import)
+      expect(delete_request).to have_been_requested
+      expect(asset.state).to eq("removed")
     end
 
     it "should redirect live images to their content publisher equivalents" do
@@ -67,6 +103,21 @@ RSpec.describe WhitehallImporter::MigrateAssets do
       described_class.call(whitehall_import)
       expect(redirect_request).to have_been_requested
       expect(asset.state).to eq("redirected")
+    end
+
+    it "should delete live image variants that have no content publisher equivalent" do
+      image_asset = create(:image_asset, state: "live")
+      asset = build(:whitehall_migration_asset_import,
+                    :for_image,
+                    variant: "s216",
+                    image_revision: build(:image_revision, blob_revision: image_asset.blob_revision),
+                    original_asset_url: original_asset_url)
+      whitehall_import = build(:whitehall_migration_document_import, assets: [asset])
+      delete_request = stub_asset_manager_delete_asset(asset_id)
+
+      described_class.call(whitehall_import)
+      expect(delete_request).to have_been_requested
+      expect(asset.state).to eq("removed")
     end
   end
 end
