460358962529bf3b83b0a239fdb87841eacd17a2
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2020-01-09 10:34:47 +0000

    Add a new exception IntegrityCheckError exception
    This exception allows to the proposed payload and problem list to be propagated
    back up to where the exceptions are caught.
    
    This exception will be raised when the integrity check fails in a later
    commit.

---
+++ lib/whitehall_importer/integrity_check_error.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module WhitehallImporter
+  class IntegrityCheckError < AbortImportError
+    attr_reader :problems, :payload
+
+    def initialize(state, integrity_check)
+      @problems = integrity_check.problems
+      @payload = integrity_check.payload
+      super("#{state.titleize} integrity check failed")
+    end
+  end
+end
