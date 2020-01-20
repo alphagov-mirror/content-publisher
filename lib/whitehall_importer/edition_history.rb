d019e03d0e7c5e86eff72f95971a59ec11f4c62a
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2020-01-20 15:28:54 +0000

    Add method for mapping Whitehall revision history
    Whitehall editions have multiple revision history entries that reflect
    changes in the document state.  This adds a method which enables us to
    get the Content Publisher equivalent from a given Whitehall revision
    history entry.

--- lib/whitehall_importer/edition_history.rb
+++ lib/whitehall_importer/edition_history.rb
@@ -18,6 +18,17 @@ module WhitehallImporter
       revision_history.select { |h| h["state"] == state }.first
     end
 
+    def previous_event(event)
+      index = revision_history.index(event)
+      return unless index
+
+      revision_history[index - 1]
+    end
+
+    def previous_event!(event)
+      previous_event(event) || (raise AbortImportError, "Edition is missing previous event for #{event}")
+    end
+
     def next_event(event)
       index = revision_history.index(event)
       return unless index
@@ -62,6 +73,27 @@ module WhitehallImporter
 
     def editors
       revision_history.pluck("whodunnit").uniq
+    end
+
+    def timeline_entry(event, edition_number)
+      if event["event"] == "create" && edition_number == 1
+        "first_created"
+      elsif event["event"] == "create" && edition_number > 1
+        "new_edition"
+      elsif previous_event(event) && previous_event(event)["state"] == event["state"]
+        "document_updated"
+      else
+        whitehall_state_mapping = {
+          "archived" => "archived",
+          "draft" => "removed",
+          "published" => "published",
+          "rejected" => "rejected",
+          "scheduled" => "scheduled",
+          "submitted" => "submitted",
+          "withdrawn" => "withdrawn",
+        }
+        whitehall_state_mapping[event["state"]] || raise(AbortImportError, "Edition history has an unsupported state")
+      end
     end
 
   private
