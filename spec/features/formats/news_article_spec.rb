7a55b40f2d31649838c2bf46de917562a0715f97
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2020-02-03 15:00:52 +0000

    Change 'news_article_spec.rb' to focus on preview rather than publish
    We only care that a user can create a document from scratch and take
    the minimal steps necessary to make the document valid against its
    schema. This validation alone should give us confidence that it can
    preview (and publish) correctly.
    
    Previewing doesn't require a body (publishing does), so I've removed
    the 'fill_in body' code from the previous commit. It does not need
    any tagging either, and therefore no publishing API stubbing in the
    'before' block.
    
    This also meant I could remove the 'reset_executed_requests!' hack.

--- spec/features/formats/news_article_spec.rb
+++ spec/features/formats/news_article_spec.rb
@@ -3,16 +3,10 @@
 RSpec.describe "News article format" do
   include TopicsHelper
 
-  before do
-    stub_any_publishing_api_put_content
-    stub_any_publishing_api_no_links
-  end
-
   scenario do
     when_i_choose_this_document_type
     and_i_fill_in_the_form_fields
-    and_i_add_some_tags
-    then_the_document_should_be_publishable
+    then_the_document_should_be_previewable
   end
 
   def when_i_choose_this_document_type
@@ -25,38 +19,16 @@ RSpec.describe "News article format" do
   end
 
   def and_i_fill_in_the_form_fields
-    fill_in "revision[title]", with: "A great title"
-    fill_in "revision[summary]", with: "A great summary"
-    fill_in "revision[contents][body]", with: "Some body content"
-
     base_path = Edition.last.document_type.path_prefix + "/a-great-title"
     stub_publishing_api_has_lookups(base_path => Document.last.content_id)
-
+    fill_in "revision[title]", with: "A great title"
+    fill_in "revision[summary]", with: "A great summary"
     click_on "Save"
   end
 
-  def and_i_add_some_tags
-    # need to stub all linkables even though we're only selecting one
-    stub_publishing_api_has_linkables([linkable], document_type: "topical_event")
-    stub_publishing_api_has_linkables([linkable], document_type: "world_location")
-    stub_publishing_api_has_linkables([linkable], document_type: "organisation")
-    stub_publishing_api_has_linkables([linkable], document_type: "role_appointment")
-
-    click_on "Change Tags"
-    select linkable["internal_name"], from: "tags[primary_publishing_organisation][]"
-
-    reset_executed_requests! # needed as we only care about the final request
-    click_on "Save"
-  end
-
-  def then_the_document_should_be_publishable
+  def then_the_document_should_be_previewable
     expect(a_request(:put, /content/).with { |req|
              expect(req.body).to be_valid_against_publisher_schema("news_article")
            }).to have_been_requested
-    expect(page).to have_link("Publish", href: publish_confirmation_path(Document.last))
-  end
-
-  def linkable
-    @linkable ||= { "content_id" => SecureRandom.uuid, "internal_name" => "Linkable" }
   end
 end
