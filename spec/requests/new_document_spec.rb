33992b29d348f7545b634bd5c92fc6f0bcb8aea2
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2020-02-07 16:00:20 +0000

    Add request tests for the "choose" and "select" routes
    At the moment, the values being selected are hard coded and depend on the YAML not changing.
    In a future commit, we'll need to create factories to make these tests, and the interactor tests less brittle.

--- spec/requests/new_document_spec.rb
+++ spec/requests/new_document_spec.rb
@@ -63,4 +63,50 @@ RSpec.describe "New Document" do
       )
     end
   end
+
+  describe "GET /documents/choose" do
+    it "shows the root document type selection when no selection has been made" do
+      get choose_path
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to have_content("Root")
+    end
+
+    it "shows the page for the selected document type" do
+      get choose_path, params: { document_type_selection_id: "news" }
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to have_content("News")
+    end
+  end
+
+  describe "POST /documents/select" do
+    it "redirects to document edit content when a content publisher managed document type is selected" do
+      post select_path, params: { document_type_selection_id: "news", selected_option_id: "news_story" }
+
+      expect(response).to redirect_to(content_path(Document.last))
+      follow_redirect!
+      expect(response.body).to have_content("news story")
+    end
+
+    it "redirects when a document type managed elsewhere is selected" do
+      post select_path, params: { document_type_selection_id: "root", selected_option_id: "not-sure" }
+
+      expect(response).to redirect_to(guidance_url)
+    end
+
+    it "asks the user to refine their selection when the document type has subtypes" do
+      post select_path, params: { document_type_selection_id: "root", selected_option_id: "news" }
+
+      expect(response).to have_http_status(:success)
+      expect(response.body).to have_content("News")
+    end
+
+    it "returns an unprocessable response with an issue when a document type isn't selected" do
+      post select_path, params: { document_type_selection_id: "news" }
+
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(response.body).to have_content(
+        I18n.t!("requirements.selected_option_id.not_selected.form_message"),
+      )
+    end
+  end
 end
