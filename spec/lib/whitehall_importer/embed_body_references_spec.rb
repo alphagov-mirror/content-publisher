750057ef5fce08e275e88a960f3f617e5bb112a2
Graham Lewis <graham.lewis@digital.cabinet-office.gov.uk>
2020-02-10 14:35:20 +0000

    Fix the import of embedded files in news articles
    When migrating two of NDA's news articles from Whitehall to Content
    Publisher, we encounter integrity check failures on the body.  The
    cause of this is the presence of image markdown in the proposed
    payload which we have failed to convert to an actual image, i.e.
    we're sending '[Image:wood_640_x_960.jpg]' rather than '<img ....'.
    
    The documents are:
    ID: 431866 Content ID: 809d70b1-3e9b-4833-a28b-41366a925805
    ID: 370367 Content ID: 5a88536b-7831-4184-b5fd-c40275ba7558
    
    It is expeced that a similar situation will occur with attachments
    due to the similarity of their handling.
    
    Files are defined in GovSpeak as !!2 for images and !@2 for
    attachments.  Both require two new lines to be entered prior to the
    file (e.g. \n\n!!2 or \n\n!@2) in order to be correctly converted
    into HTML correctly.
    
    We need to fix the import so these files are correctly parsed,
    and the proposed payload for Publishing API matches the currently
    published document.

--- spec/lib/whitehall_importer/embed_body_references_spec.rb
+++ spec/lib/whitehall_importer/embed_body_references_spec.rb
@@ -12,31 +12,96 @@ RSpec.describe WhitehallImporter::EmbedBodyReferences do
       expect(govspeak_body).to eq("[Contact:#{content_id}]")
     end
 
-    it "converts Whitehall image syntax to Content Publisher syntax" do
-      govspeak_body = described_class.call(
+    it "converts Whitehall image bang embeds to govspeak Image embeds" do
+      body = described_class.call(body: "!!1", images: ["file.jpg"])
+
+      expect(body).to eq("[Image:file.jpg]")
+    end
+
+    it "converts Whitehall attachment bang embeds to govspeak Attachment embeds" do
+      body = described_class.call(body: "!@1", attachments: ["file.pdf"])
+
+      expect(body).to eq("[Attachment:file.pdf]")
+    end
+
+    it "ignore Whitehall image bang embeds that are neither start of the string or preceeded by new lines" do
+      body = described_class.call(
         body: "!!1 test !!2",
-        images: ["foo.png", "bar.jpg"],
+        images: ["file.png", "file.jpg"],
       )
 
-      expect(govspeak_body).to eq("[Image:foo.png] test [Image:bar.jpg]")
+      expect(body).to eq("[Image:file.png] test !!2")
     end
 
-    it "removes any image markdown that doesn't resolve to an image" do
-      govspeak_body = described_class.call(
-        body: "Bar !!2 Baz",
-        images: ["foo.png"],
-      )
-
-      expect(govspeak_body).to eq("Bar  Baz")
-    end
-
-    it "converts Whitehall attachment syntax to Content Publisher syntax" do
-      govspeak_body = described_class.call(
+    it "ignore Whitehall attachment bang embeds that are neither start of the string or preceeded by new lines" do
+      body = described_class.call(
         body: "!@1 test !@2",
-        attachments: ["file.pdf", "download.csv"],
+        attachments: ["file.pdf", "file.csv"],
       )
 
-      expect(govspeak_body).to eq("[Attachment:file.pdf] test [Attachment:download.csv]")
+      expect(body).to eq("[Attachment:file.pdf] test !@2")
+    end
+
+    it "ignore any image markdown that doesn't resolve to an image" do
+      body = described_class.call(
+        body: "Bar !!2 Baz",
+        images: ["file.jpg"],
+      )
+
+      expect(body).to eq("Bar !!2 Baz")
+    end
+
+    it "ignore any attachment markdown that doesn't resolve to an attachment" do
+      body = described_class.call(
+        body: "Bar !@2 Baz",
+        attachments: ["file.pdf"],
+      )
+
+      expect(body).to eq("Bar !@2 Baz")
+    end
+
+    it "converts image and attachment embeds prefixed by single new lines to be prefixed by two" do
+      prefix_conversions = [
+        { prefix: "\n", conversion: "\n\n" },
+        { prefix: "\r\n", conversion: "\r\n\r\n" },
+      ]
+
+      prefix_conversions.each do |prefix_conversion|
+        prefixed = []
+        prefixed << "#{prefix_conversion[:prefix]}!!1"
+        prefixed << "#{prefix_conversion[:prefix]}!@1"
+
+        converted = []
+        converted << "#{prefix_conversion[:conversion]}[Image:file.jpg]"
+        converted << "#{prefix_conversion[:conversion]}[Attachment:file.pdf]"
+
+        body = described_class.call(
+          body: prefixed.join,
+          images: ["file.jpg"],
+          attachments: ["file.pdf"],
+        )
+
+        expect(body).to eq(converted.join)
+      end
+    end
+
+    it "doesn't change the prefixes of images and attachments preceeded by two or more new lines" do
+      prefixes = [
+        "\n\n", "\r\n\r\n",
+        "\n\n\n", "\r\n\r\n\r\n",
+        "\n\n\n\n", "\r\n\r\n\r\n\r\n"
+      ]
+
+      prefixes.each do |prefix|
+        body = described_class.call(
+          body: "#{prefix}!!1#{prefix}!@1",
+          images: ["file.jpg"],
+          attachments: ["file.pdf"],
+        )
+
+        expect(body)
+          .to eq("#{prefix}[Image:file.jpg]#{prefix}[Attachment:file.pdf]")
+      end
     end
 
     it "converts Whitehall inline attachment syntax to Content Publisher syntax" do
@@ -45,16 +110,8 @@ RSpec.describe WhitehallImporter::EmbedBodyReferences do
         attachments: ["file.pdf", "download.csv"],
       )
 
-      expect(govspeak_body).to eq("[AttachmentLink:file.pdf] test [AttachmentLink:download.csv]")
-    end
-
-    it "removes any attachment markdown that doesn't resolve to an attachment" do
-      govspeak_body = described_class.call(
-        body: "Bar !@2 Baz [InlineAttachment:3] << removed, but !@1 continues to work",
-        attachments: ["foo.pdf"],
-      )
-
-      expect(govspeak_body).to eq("Bar  Baz  << removed, but [Attachment:foo.pdf] continues to work")
+      expect(govspeak_body)
+        .to eq("[AttachmentLink:file.pdf] test [AttachmentLink:download.csv]")
     end
   end
 end
