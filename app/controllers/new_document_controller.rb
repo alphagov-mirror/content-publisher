058814071a9ce52d80c14cdfec0966971059a1d7
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2020-02-12 12:23:36 +0000

    Add new routes for show and select document
    The existing routes will be removed in a later commit.
    Adding new routes allows them to be tested in isolation as they're built, without breaking
    other feature and unit tests.

--- app/controllers/new_document_controller.rb
+++ app/controllers/new_document_controller.rb
@@ -1,4 +1,8 @@
 class NewDocumentController < ApplicationController
+  rescue_from DocumentTypeSelection::NotFoundError do |e|
+    raise ActionController::RoutingError, e.message
+  end
+
   def choose_document_type
     result = NewDocument::ChooseSupertypeInteractor.call(params: params, user: current_user)
     issues, @supertype = result.to_h.values_at(:issues, :supertype)
@@ -29,6 +33,34 @@ class NewDocumentController < ApplicationController
              status: :unprocessable_entity
     elsif result.managed_elsewhere
       redirect_to document_type.managed_elsewhere_url
+    else
+      redirect_to content_path(document)
+    end
+  end
+
+  def show
+    @document_type_selection = DocumentTypeSelection.find(params[:type] || "root")
+  end
+
+  def select
+    result = NewDocument::SelectInteractor.call(params: params, user: current_user)
+    issues, document, redirect_url, document_type_selection, current_selection = result.to_h.values_at(
+      :issues,
+      :document,
+      :redirect_url,
+      :document_type_selection,
+      :current_selection,
+    )
+
+    if issues
+      flash.now["requirements"] = { "items" => issues.items }
+      render :show,
+             assigns: { issues: issues, document_type_selection: document_type_selection },
+             status: :unprocessable_entity
+    elsif current_selection
+      redirect_to show_path(type: current_selection.id)
+    elsif redirect_url
+      redirect_to redirect_url
     else
       redirect_to content_path(document)
     end
