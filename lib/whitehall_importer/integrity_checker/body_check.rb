9770b5d51c988ea461501827c411a64d6c2ea35d
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2020-02-12 09:15:59 +0000

    Do not compare inline attachment file size in integrity check
    There are differences between the file size format for inline attachments
    published in Whitehall compared to those published in Content Publisher.
    We should therefore not compare these when performing the integrity check.

--- lib/whitehall_importer/integrity_checker/body_check.rb
+++ lib/whitehall_importer/integrity_checker/body_check.rb
@@ -11,7 +11,19 @@ module WhitehallImporter
       proposed_body_text = proposed_payload.dig(:details, :body)
       publishing_api_body_text = publishing_api_content.dig(:details, :body)
 
+      content_publisher_file_size_selector = ".gem-c-attachment-link .gem-c-attachment-link__attribute:nth-of-type(2)"
+      whitehall_file_size_selector = ".attachment-inline .file-size, .gem-c-attachment-link .gem-c-attachment-link__attribute:nth-of-type(2)"
+      proposed_body_text = remove_html_elements(proposed_body_text, content_publisher_file_size_selector)
+      publishing_api_body_text = remove_html_elements(publishing_api_body_text, whitehall_file_size_selector)
       Sanitize.clean(publishing_api_body_text).squish == Sanitize.clean(proposed_body_text).squish
+    end
+
+    def remove_html_elements(body, selector)
+      doc = Nokogiri.HTML(body)
+      doc.search(selector).each do |el|
+        el.replace("")
+      end
+      doc.to_html
     end
   end
 end
--- spec/lib/whitehall_importer/integrity_checker/body_check_spec.rb
+++ spec/lib/whitehall_importer/integrity_checker/body_check_spec.rb
@@ -33,6 +33,14 @@ RSpec.describe WhitehallImporter::IntegrityChecker::BodyCheck do
       expect(integrity_check.body_text_matches?).to be true
     end
 
+    it "returns no problems even if there is a mismatch in inline atttachment URL filesize" do
+      proposed_payload[:details][:body] = "<p>Some other text</p><p><span class=\"gem-c-attachment-link\"><a class=\"govuk-link\" href=\"filename.pdf\" target=\"_blank\">Test File</a> (<span class=\"gem-c-attachment-link__attribute\"><abbr title=\"Portable Document Format\" class=\"gem-c-attachment-link__abbr\">PDF</abbr></span>, <span class=\"gem-c-attachment-link__attribute\">391 KB</span>, <span class=\"gem-c-attachment-link__attribute\">9 pages</span>)</span></p>"
+      publishing_api_content[:details][:body] = "<p>Some other text</p><p><span class=\"attachment-inline\"><a href=\"/filename.pdf\">Test File</a> (<span class=\"type\">PDF</span>, <span class=\"file-size\">391KB</span>, <span class=\"page-length\">9 pages</span>)</span></p>"
+
+      integrity_check = WhitehallImporter::IntegrityChecker::BodyCheck.new(proposed_payload, publishing_api_content)
+      expect(integrity_check.body_text_matches?).to be true
+    end
+
     it "returns a problem when the body text doesn't match" do
       proposed_payload[:details][:body] = "Some text"
       publishing_api_content[:details][:body] = "Some different text"
