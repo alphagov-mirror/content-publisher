53f114179d1c68af069eb34b900a1cc38a19c5a8
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2020-02-12 12:23:36 +0000

    Add new routes for show and select document
    The existing routes will be removed in a later commit.
    Adding new routes allows them to be tested in isolation as they're built, without breaking
    other feature and unit tests.

--- app/controllers/new_document_controller.rb
+++ app/controllers/new_document_controller.rb
@@ -33,4 +33,32 @@ class NewDocumentController < ApplicationController
       redirect_to content_path(document)
     end
   end
+
+  def show
+    @document_type_selection = DocumentTypeSelection.find(params[:document_type_selection_id] || "root")
+  end
+
+  def select
+    result = NewDocument::DocumentTypeSelectionInteractor.call(params: params, user: current_user)
+    issues, document, redirect_url, previous_selection, current_selection = result.to_h.values_at(
+      :issues,
+      :document,
+      :redirect_url,
+      :previous_selection,
+      :current_selection,
+    )
+
+    if issues
+      flash.now["requirements"] = { "items" => issues.items }
+      render :show,
+             assigns: { issues: issues, document_type_selection: previous_selection },
+             status: :unprocessable_entity
+    elsif current_selection
+      redirect_to show_path(document_type_selection_id: current_selection.id)
+    elsif redirect_url
+      redirect_to redirect_url
+    else
+      redirect_to content_path(document)
+    end
+  end
 end
